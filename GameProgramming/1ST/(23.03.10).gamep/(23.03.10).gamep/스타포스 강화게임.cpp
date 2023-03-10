eeedb06d70bb46eb4f3e118b72ad9c0774ac0ef5
DDongYeop <kitty-777@naver.com>
2023-03-10 13:19:16 +0900

    게임프로그래밍 - 스타포스 강화게임 완성

--- GameProgramming/1ST/(23.03.10).gamep/(23.03.10).gamep/스타포스 강화게임.cpp
+++ GameProgramming/1ST/(23.03.10).gamep/(23.03.10).gamep/스타포스 강화게임.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main()
 {
+	srand((unsigned int)time(NULL));
 	cout << "=================" << endl;
 	cout << "스타포스 강화 게임" << endl;
 	cout << "=================" << endl << endl;
@@ -11,6 +12,28 @@ int main()
 	float fPercent = 0.f;
 	cout << "현재 무기는 몇성인가요?" << endl;
 	cin >> iUpgrade;
+	switch (iUpgrade)
+	{
+	case 0:
+	case 1:
+	case 2:
+		fPercent = 90;
+		break;
+	case 3:
+	case 4:
+	case 5:
+		fPercent = 50;
+		break;
+	case 6:
+	case 7:
+	case 8:
+	case 9:
+		fPercent = 10;
+		break;
+	default:
+		fPercent = 1;
+		break;
+	}
 	cout << "현재 무기 UPgrade : " << iUpgrade << "성" << endl;
 	cout << "강화 확률 Percent : " << fPercent << "%" << endl;
 	cout << "강화하시겠습니까? 예:1 , 아니오: 0" << endl;
@@ -19,4 +42,9 @@ int main()
 
 	if (iInput == 0) //종료조건 처리.
 		return 0;
+
+	if (fPercent > rand() % 101 + 1)
+		cout << "강화 성공";
+	else
+		cout << "강화 실패";
 }
