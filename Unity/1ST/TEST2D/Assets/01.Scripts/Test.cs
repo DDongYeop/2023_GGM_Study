d3eece0cd579506950296a5906a8a1e4dd0d47fd
DDongYeop <kitty-777@naver.com>
2023-03-26 14:44:32 +0900

    1학기 방과후 엔진 - Interface, 충돌

--- Unity/1ST/TEST2D/Assets/01.Scripts/Test.cs
+++ Unity/1ST/TEST2D/Assets/01.Scripts/Test.cs
@@ -1,7 +1,13 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+
+public interface Human
+{
+    public bool PassCertificate();
+}
 
 public class Student
 {
@@ -19,17 +25,24 @@ public class Student
     // public, private, protected, internal >> 같은 어셈블리 안에서 통한다  
 }
 
-public class Student2 : Student
+public class Student2 : Student, Human
 {
     public override void Introduce() //수정을 위해 overriade를 해줘야한다 
     {
         //base.Introduce();
         Debug.Log("안녕하세요. 2학년 학생입니다.");
     }
+
+    public bool PassCertificate()
+    {
+        return true;
+    }
 }
 
 public class Test : MonoBehaviour
 {
+    private Collision2D _col;
+    
     public void PrintMsg()
     {
         //GC.Collect(); //수동으로 가비지컬렋터 작동
@@ -38,5 +51,22 @@ public class Test : MonoBehaviour
         s.Introduce();
         Student2 s2 = new Student2();
         s2.Introduce();
+        Human s3 = new Student2();
+        if (s3.PassCertificate())
+            Debug.Log("정처기합격. 난 자유.");
+        else
+            Debug.Log("난 망함. 떨어짐 ㅂㅂ");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _col = collision;
+        Debug.Log("충돌");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            Debug.Log(_col.gameObject.name);
     }
 }
