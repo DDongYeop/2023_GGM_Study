a69f379b6c2a9a4db7c9374ff964bcbc35debd69
DDongYeop <kitty-777@naver.com>
2023-04-07 12:58:55 +0900

    게임프로그래밍 - 입력, 움직이는거

--- GameProgramming/1ST/(23.04.06).gamep/(23.04.06).gamep/Main.cpp
+++ GameProgramming/1ST/(23.04.06).gamep/(23.04.06).gamep/Main.cpp
@@ -30,7 +30,106 @@ int main()
 		//system("pause");
 	}
 
-	Gotoxy(10, 10);
+	{
+		/*Gotoxy(10, 10);
+
+		SetColor(14, 7);
+		cout << "I wnat go home";
+
+		Cursorset(false, 1);*/
+	}
+
+	{
+	/*	Gotoxy(10, 10);
+		cout << "Hello World!";
+		Sleep(1000);
+		int oldcolor = GetColor() | (GetbgColor() << 4);
+		for (int i = 0; i < 16; ++i)
+		{
+			Gotoxy(i, i);
+			SetColor(i, 0);
+			cout << "color number : ";
+			SetColor(oldcolor, oldcolor >> 4);
+			cout << i;
+		}*/
+	}
+	
+	int test;
+	int x = 0, y = 0;
+	int beforex = 0, beforey = 0;
+	
+	// 스크린 조절 못 하게 윈11 안됨
+	HWND console = GetConsoleWindow();
+	SetWindowLong(console, GWL_STYLE, GetWindowLong(console, GWL_STYLE) & WS_SIZEBOX);
+	RECT rt;
+	GetWindowRect(console, &rt);
+	while (false)
+	{
+		//if (_kbhit())
+		//{
+		//	test = _getch();
+		//	if (test == 224)
+		//	{
+		//		test = _getch();
+		//		switch (test)
+		//		{
+		//		case 72:
+		//			//cout << "위쪽" << endl;
+		//			y--;
+		//			break;
+		//		case 75:
+		//			//cout << "왼쪽" << endl;
+		//			x--;
+		//			break;
+		//		case 77:
+		//			//cout << "오른쪽" << endl;
+		//			x++;
+		//			break;
+		//		case 80:
+		//			//cout << "아래쪽" << endl;
+		//			y++;
+		//			break;
+		//		}
+		//	}
+		//	GotoxyPlayer(x, y);
+		//	_putch('a');
+		//}
+
+		if (GetAsyncKeyState(VK_UP)	   & 0x8000) y--;
+		if (GetAsyncKeyState(VK_DOWN)  & 0x8000) y++;
+		if (GetAsyncKeyState(VK_LEFT)  & 0x8000) x--;
+		if (GetAsyncKeyState(VK_RIGHT) & 0x8000) x++;
+		Sleep(50);
+		BOOL result = GotoxyTest(x, y);
+		if (result == FALSE)
+		{
+			x = beforex;
+			y = beforey;
+			continue;
+		}
+		else
+		{
+			beforex = x;
+			beforey = y;
+		}
+		GotoxyPlayer(x, y);
+		_putch('a');
+	}
+
+	int a;
+	while (true)
+	{
+		cout << "숫자만 입력하세요. " << endl;
+		cin >> a;
+		if (cin.fail())
+		{
+			cout << "너 비정상임 ㅋㅋ " << endl;
+			cin.clear();
+			cin.ignore(INT_MAX, '\n');
+			// clear(), ignore()
+		}
+		cout << a << endl;
+	}
 
 	_getch();
 }
--- GameProgramming/1ST/(23.04.06).gamep/(23.04.06).gamep/console.h
+++ GameProgramming/1ST/(23.04.06).gamep/(23.04.06).gamep/console.h
@@ -1,3 +1,13 @@
 #pragma once
+enum class COLOR
+{
+	BALCK, BLUE, GREEN, SKYBLUE, RED, VIOLET, YELLOW, LIGHT_GRAY, GRAY, LIGHT_BLUE, LIGHT_GREEN, MINT, LIGHT_RED, LIGHY_VIOLET, LIGHT_YELLOW, WHITE
+};
 void FullScreen();
 void Gotoxy(int, int);
+void GotoxyPlayer(int x, int y);
+BOOL GotoxyTest(int, int);
+void Cursorset(bool, DWORD);
+void SetColor(int color, int bgcolor);
+int GetColor();
+int GetbgColor();
