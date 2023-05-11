1ccd4897e4a9a85d7d8e936e2ec26dc814b75e21
DDongYeop <kitty-777@naver.com>
2023-05-11 15:22:01 +0900

    네트워크 에러처리

--- Network/1ST/20230511/app.js
+++ Network/1ST/20230511/app.js
@@ -1,13 +1,40 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 //서버에다가 변수 설정
 app.set('port', 3000);
 
-app.get('/', (req, res) =>
+app.use((req, res, next) =>
 {
-    res.send('Hello 김민수');
+    console.log('모든 요청에 다 실행됩니다.');
+    next();
+})
+
+app.get('*');
+
+app.get('/', (req, res, next) =>
+{
+    console.log('Get /요청에서만 실행됩니다.');
+    //res.send('Hello 김민수');
+    res.sendFile(path.join(__dirname, '/index.html'));
 });
+
+app.get('/category/park', (req, res) =>
+{
+    res.send(`부성아 왜 그랫어`);
+})
+
+app.get('/category/:name', (req, res) =>
+{
+    res.send(`hello ${req.params.name}`);
+});
+
+app.use((err, req, res, next) =>
+{
+    console.err(err);
+    res.send('에러남');
+})
 
 app.listen(app.get('port'), () =>
 {
