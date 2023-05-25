5e569cf9a3c1c7b8f1cdc2cd308295e5648f4ffe
DDongYeop <kitty-777@naver.com>
2023-05-25 10:05:02 +0900

    수학 - 시야각 구현

--- Math/1ST/ViewingAngle/Assets/01.Scripts/ViewAngle.cs
+++ Math/1ST/ViewingAngle/Assets/01.Scripts/ViewAngle.cs
@@ -1,24 +1,18 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class ViewAngle : MonoBehaviour
 {
-    [SerializeField] private bool _bDebugMode = false;
- 
-    [Header("View Config")] [Range(0f, 360f)] [SerializeField] private float _horizontalViewAngle = 0f;
+    [Header("View Config")] 
     [SerializeField] private float _viewRadius = 1f;
-    [Range(-180f, 180f)] [SerializeField] private float _viewRotateZ = 0f;
- 
-    private List<Collider2D> _hitedTargetContainer = new List<Collider2D>();
- 
+    [Range(0f, 360f)] [SerializeField] private float _horizontalViewAngle = 0f;
     private float _horizontalViewHalfAngle = 0f;
- 
+
+    private SpriteRenderer _spriteRenderer;
+    
     private void Awake()
     {
         _horizontalViewHalfAngle = _horizontalViewAngle * 0.5f;
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
     
     private Vector3 AngleToDirZ(float angleInDegree)
@@ -29,58 +23,52 @@ public class ViewAngle : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        if (_bDebugMode)
-        {
-            _horizontalViewHalfAngle = _horizontalViewAngle * 0.5f;
+        Vector3 originPos = transform.position;
  
-            Vector3 originPos = transform.position;
- 
-            Gizmos.DrawWireSphere(originPos, _viewRadius);
- 
-            Vector3 horizontalRightDir = AngleToDirZ(-_horizontalViewHalfAngle + _viewRotateZ);
-            Vector3 horizontalLeftDir  = AngleToDirZ(_horizontalViewHalfAngle + _viewRotateZ);
-            Vector3 lookDir = AngleToDirZ(_viewRotateZ);
- 
-            Debug.DrawRay(originPos, horizontalLeftDir * _viewRadius, Color.cyan);
-            Debug.DrawRay(originPos, lookDir * _viewRadius, Color.green);
-            Debug.DrawRay(originPos, horizontalRightDir * _viewRadius, Color.cyan);
+        Gizmos.DrawWireSphere(originPos, _viewRadius);
 
-            FindViewTargets();
-        }
+        float eulerAngleZ = transform.eulerAngles.z;
+        Vector3 horizontalRightDir = AngleToDirZ(-_horizontalViewHalfAngle + eulerAngleZ);
+        Vector3 horizontalLeftDir  = AngleToDirZ(_horizontalViewHalfAngle + eulerAngleZ);
+        Vector3 lookDir = AngleToDirZ(eulerAngleZ);
+ 
+        Debug.DrawRay(originPos, horizontalLeftDir * _viewRadius, Color.cyan);
+        Debug.DrawRay(originPos, lookDir * _viewRadius, Color.green);
+        Debug.DrawRay(originPos, horizontalRightDir * _viewRadius, Color.cyan);
+
+        FindViewTarget();
     }
     
-    public void FindViewTargets()
+    public void FindViewTarget()
     {
-        _hitedTargetContainer.Clear();
- 
-        Vector2      originPos    = transform.position;
-        Collider2D[] hitedTargets = Physics2D.OverlapCircleAll(originPos, _viewRadius);
-        
-        foreach (Collider2D hitedTarget in hitedTargets)
-        {
-            Vector2 targetPos = hitedTarget.transform.position;
-            Vector2 dir = (targetPos - originPos).normalized;
-            Vector2 lookDir = AngleToDirZ(_viewRotateZ);
-            
-            float dot = Vector2.Dot(lookDir, dir);
-            float angle = Mathf.Acos(dot) * Mathf.Rad2Deg;
- 
-            if (angle <= _horizontalViewHalfAngle)
-            {
-                RaycastHit2D rayHitedTarget = Physics2D.Raycast(originPos, dir, _viewRadius);
-                if (rayHitedTarget)
-                {
-                    if (_bDebugMode)
-                        Debug.DrawLine(originPos, rayHitedTarget.point, Color.yellow);
-                }
-                else
-                {
-                    _hitedTargetContainer.Add(hitedTarget);
-                    if (_bDebugMode)
-                        Debug.DrawLine(originPos, targetPos, Color.red);
-                }
-            }
-        }
-    }
+        Vector2 originPos = transform.position;
+        Collider2D hitedTarget = Physics2D.OverlapCircle(originPos, _viewRadius);
 
+        if (hitedTarget == null)
+        {
+            _spriteRenderer.color = Color.white;
+            return;
+        }
+        
+        Vector2 targetPos = hitedTarget.transform.position;
+        Vector2 dir = (targetPos - originPos).normalized;
+        Vector2 lookDir = AngleToDirZ(transform.eulerAngles.z);
+        
+        float dot = Vector2.Dot(lookDir, dir);
+        float angle = Mathf.Acos(dot) * Mathf.Rad2Deg;
+        
+        if (angle <= _horizontalViewHalfAngle)
+        {
+            Debug.DrawLine(originPos, hitedTarget.transform.position, Color.yellow);
+            _spriteRenderer.color = Color.red;
+        }
+        else if (angle >= 90)
+        {
+            _spriteRenderer.color = Color.green;
+        }
+        else
+        {
+            _spriteRenderer.color = Color.white;
+        }   
+    }
 }
