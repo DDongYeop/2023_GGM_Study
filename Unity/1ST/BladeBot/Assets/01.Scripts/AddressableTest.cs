7ba27f205c2a57157e80b7f9b0731b4202303161
DDongYeop <kitty-777@naver.com>
2023-07-12 10:59:28 +0900

    엔진 - Adressable

--- Unity/1ST/BladeBot/Assets/01.Scripts/AddressableTest.cs
+++ Unity/1ST/BladeBot/Assets/01.Scripts/AddressableTest.cs
@@ -3,21 +3,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class AddressableTest : MonoBehaviour
 {
+    [SerializeField] private AssetReference _ref;
+    
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.A))
             LoadPopupText();
+        if (Input.GetKeyDown(KeyCode.D))
+            UnloadPopupText();
     }
+
+    private AsyncOperationHandle<GameObject> _handle;
+    private GameObject _popupText;
     
     private void LoadPopupText()
     {
-        Addressables.LoadAssetAsync<GameObject>($"PopupText").Completed += obj =>
+        _ref.InstantiateAsync(Vector3.zero, Quaternion.identity).Completed += (obj) =>
         {
-            Debug.Log(obj);
-            Debug.Log(obj.Result);
+            _handle = obj;
+            _popupText = obj.Result;
         };
+    }
+
+    private void UnloadPopupText()
+    { 
+        _ref.ReleaseInstance(_popupText);
     }
 }
