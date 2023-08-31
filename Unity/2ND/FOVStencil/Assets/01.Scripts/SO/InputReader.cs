376541bc215c1a4a31b7d984bff1e32e0df83a11
DDongYeop <kitty-777@naver.com>
2023-08-31 15:39:17 +0900

    엔진 - Stancil Add Project

---
+++ Unity/2ND/FOVStencil/Assets/01.Scripts/SO/InputReader.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static Controls;
+
+public class InputReader : ScriptableObject, IPlayerActions
+{
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+        
+    }
+
+    public void OnAIM(InputAction.CallbackContext context)
+    {
+        
+    }
+}
