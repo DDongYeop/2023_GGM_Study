2e10b49a8d12e045bdaee025b75712aea4739c79
DDongYeop <kitty-777@naver.com>
2023-09-01 09:58:51 +0900

    엔진 - FOV

--- Unity/2ND/FOVStencil/Assets/01.Scripts/SO/InputReader.cs
+++ Unity/2ND/FOVStencil/Assets/01.Scripts/SO/InputReader.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using static Controls;
 
+[CreateAssetMenu(menuName = "SO/InputRender")]
 public class InputReader : ScriptableObject, IPlayerActions
 {
+    public event Action<Vector2> Movement;
+    public Vector2 AimPosition
+    {
+        get;
+        private set;
+    }
+
+    private Controls _controls;
+
+    private void OnEnable()
+    {
+        if (_controls == null)
+        {
+            _controls = new Controls();
+            _controls.Player.SetCallbacks(this);
+        }
+        
+        _controls.Player.Enable();
+    }
+
     public void OnMovement(InputAction.CallbackContext context)
     {
-        
+        Vector2 value = context.ReadValue<Vector2>();
+        Movement?.Invoke(value); //노멀라이즈 된 값이다
     }
 
     public void OnAIM(InputAction.CallbackContext context)
     {
-        
+        AimPosition = context.ReadValue<Vector2>();
     }
 }
