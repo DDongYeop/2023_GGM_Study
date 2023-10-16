a8e30b2c5b2ad68877db2981a28614ee9fe00a10
Dongyeop <ddongyeop0129@gmail.com>
2023-10-16 12:57:50 +0900

    엔진 - Addressable

---
+++ Unity/2ND/School_Main/AddressableLecture/Assets/01.Scripts/TestLoader.cs
@@ -0,0 +1,30 @@
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.InputSystem;
+
+public class TestLoader : MonoBehaviour
+{
+    [SerializeField] private AssetReference _levelRef;
+
+    private void Update()
+    {
+        if (Keyboard.current.qKey.wasPressedThisFrame)
+        {
+            LoadLevel();
+        }
+    }
+
+    private async Task LoadLevel()
+    {
+        if (!_levelRef.IsValid())
+        {
+            GameObject levelPrefab = await _levelRef.LoadAssetAsync<GameObject>().Task;
+            Instantiate(levelPrefab, Vector3.zero, Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(_levelRef.Asset, Vector3.zero, Quaternion.identity);
+        }
+    }
+}
