901768a1c59d2b2304c87ab9fd1caa065c333db6
Dongyeop <ddongyeop0129@gmail.com>
2023-10-24 10:15:09 +0900

    AI - CS

---
+++ CSharp/2ND/AI AfterSchool/20231023/20231023/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20231023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //sealed << 상속 못 받게 해주는애 
+
+            //partial << 하나의 클래스로 생각
+        }
+    }
+}
