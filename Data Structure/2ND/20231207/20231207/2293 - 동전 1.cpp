743228cd30e50f34ff33754b7d4538e47bb1fe49
Dongyeop <ddongyeop0129@gmail.com>
2023-12-07 11:11:31 +0900

    자료구조 - 동전 1

---
+++ Data Structure/2ND/20231207/20231207/2293 - 동전 1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int n, k, input;
+	vector<int> vec;
+	cin >> n >> k;
+
+	while (n--)
+	{
+		cin >> input;
+		vec.push_back(input);
+	}
+}
